dc300d1c84118f9b1c7f6b618aac35aa5a75aac4
Namkhai B <namkhai.n3@protonmail.com>
2021-06-12 13:27:48 -0500

    freebsd: Remove VersionControl, AgentNews & SignIn components

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,11 +4,8 @@ import { Flex } from "@netdata/netdata-ui"
 import Item from "./item"
 import Node from "./node"
 import Options from "./options"
-import Version from "./version"
 import DateTimePicker from "./dateTimePicker"
 import Alarms from "./alarms"
-import AgentNews from "./news"
-import SignIn from "./signIn"
 
 const Wrapper = styled(Flex).attrs({
   as: "header",
@@ -29,13 +26,10 @@ const Header = () => (
     </Flex>
     <Flex justifyContent="end" alignItems="center" gap={3}>
       <Item hasBorder>
-        <Version />
-        <AgentNews />
         <Options />
       </Item>
       <DateTimePicker />
       <Alarms />
-      <SignIn />
     </Flex>
   </Wrapper>
 )
